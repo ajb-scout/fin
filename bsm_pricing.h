b8e2226142e6ac1bf013375e5e2a6b0cef314b76
ajb-scout <alexander.buckle12@gmail.com>
2024-11-18 10:43:51 +0000

    Updates

---
+++ bsm_pricing.h
@@ -0,0 +1,28 @@
+#ifndef BSM_PRICING_H
+#define BSM_PRICING_H
+
+extern "C" {
+    double black_scholes(double S, double K, double T, double r, double sigma, bool is_call);
+    double delta(double S, double K, double T, double r, double sigma, bool is_call);
+    double gamma(double S, double K, double T, double r, double sigma);
+    double vega(double S, double K, double T, double r, double sigma);
+    double theta(double S, double K, double T, double r, double sigma, bool is_call);
+    double rho(double S, double K, double T, double r, double sigma, bool is_call);
+    double norm_cdf(double x);
+    
+    void black_scholes_strike_vec(double* S, double* output, double K, double T, double r, double sigma, bool is_call, int size);
+    
+    void delta_for_price(double* S, double* output, double K, double T, double r, double sigma, bool is_call, int size);
+    void delta_for_time(double S, double* output, double K, double* T, double r, double sigma, bool is_call, int size);
+    void theta_for_price(double* S, double* output, double K, double T, double r, double sigma, bool is_call, int size);
+    void theta_for_time(double S, double* output, double K, double* T, double r, double sigma, bool is_call, int size);
+    void gamma_for_price(double* S, double* output, double K, double T, double r, double sigma, bool is_call, int size);
+    void gamma_for_time(double S, double* output, double K, double* T, double r, double sigma, bool is_call, int size);
+
+
+    void delta_for_price_time(double* S, double* output, double K, double* T, double r, double sigma, bool is_call, int sSize, int tSize );
+    void theta_for_price_time(double* S, double* output, double K, double* T, double r, double sigma, bool is_call, int sSize, int tSize);
+    void gamma_for_price_time(double* S, double* output, double K, double* T, double r, double sigma, bool is_call, int sSize, int tSize);
+
+}
+#endif // BSM_PRICING_H
